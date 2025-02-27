4d94dca9019e5fbd14bed9e98a5c8881d5712751
Willian Rocha <contato@ecorde.com.br>
2025-02-27 16:03:43 -0300

    Tratamento com exception em categorias

--- src/main/java/com/willianrocha/cursomc/services/CategoriaService.java
+++ src/main/java/com/willianrocha/cursomc/services/CategoriaService.java
@@ -1,22 +1,22 @@
 package com.willianrocha.cursomc.services;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.willianrocha.cursomc.domain.Categoria;
 import com.willianrocha.cursomc.repositories.CategoriaRepository;
+import com.willianrocha.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
-	@Autowired
-	private CategoriaRepository repo;
-	
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
-	
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+            "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())
+        );
+    }
 }
